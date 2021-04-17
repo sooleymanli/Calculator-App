04d9b9c1ac270795b98c8ab2cd82478419bd8dad
Fuad Süleymanlı <suleymanli95@hotmail.com>
2021-04-17 11:41:38 +0400

    Created

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Calculator-App
+
+
+ ### 
+ 1. [Demo](https://fuadsuleymanli.xyz/Demos/calculator-app/) 
+ 2. [Demo (Codepen)](https://codepen.io/fuads062/pen/YzNjVRw)
+
+###
+![ScreenShot](/screenshot.gif)
---
+++ script.js
@@ -0,0 +1,71 @@
+// Süleymalı Fuad
+
+function reqemSec(nomre) {
+    reqem = document.getElementById("netice").innerHTML;
+    reqem = reqem + nomre;
+    document.getElementById("netice").innerHTML = reqem;
+    document.getElementById("netice1").innerHTML = reqem;
+
+}
+
+function noqteQoy() {
+    reqem = document.getElementById("netice").innerHTML
+    reqem = reqem + "."
+    document.getElementById("netice").innerHTML = reqem;
+    document.getElementById("netice1").innerHTML = reqem;
+}
+
+function resett() {
+    reset = document.getElementById("netice").innerHTML
+    reset = "";
+    document.getElementById("netice").innerHTML = reset;
+}
+
+function azaltReqemi() {
+    reqem = document.getElementById("netice").innerHTML
+    document.getElementById("netice").innerHTML = reqem.substring(0, reqem.length - 1);
+
+
+} function emelVurma() {
+    reqem = document.getElementById("netice").innerHTML
+    reqem = reqem + "*"
+    document.getElementById("netice").innerHTML = reqem;
+    document.getElementById("netice1").innerHTML = reqem;
+
+}
+
+function emelBolme() {
+    reqem = document.getElementById("netice").innerHTML
+    reqem = reqem + "/"
+    document.getElementById("netice").innerHTML = reqem;
+    document.getElementById("netice1").innerHTML = reqem;
+
+}
+function emelCixma() {
+    reqem = document.getElementById("netice").innerHTML
+    reqem = reqem + "-"
+    document.getElementById("netice").innerHTML = reqem;
+    document.getElementById("netice1").innerHTML = reqem;
+
+}
+function emelToplama() {
+    reqem = document.getElementById("netice").innerHTML
+    reqem = reqem + "+"
+    document.getElementById("netice").innerHTML = reqem;
+    document.getElementById("netice1").innerHTML = reqem;
+
+}
+
+function yoxlama() {
+    if (isNaN(eval(neticemiz)) !== false) { alert("Səhflik var") }
+}
+
+function netice() {
+    neticemiz = document.getElementById("netice").innerHTML
+    yoxlama();
+    document.getElementById("netice1").innerHTML = neticemiz + "=" + eval(neticemiz)
+    document.getElementById("netice").innerHTML = eval(neticemiz)
+
+}
+
+
